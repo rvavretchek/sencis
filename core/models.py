4f2733a40b6e5a13f57d3c0ec98f1e5abf258378
Ricardo Vavretchek <ricardo.vavretchek@hotmail.com>
2024-04-08 09:30:35 -0300

    Aplicação de estudo do framework Django

---
+++ core/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from stdimage.models import StdImageField
+
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+
+    servico = models.CharField('Servico', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Icone', max_length=12, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return self.servico
