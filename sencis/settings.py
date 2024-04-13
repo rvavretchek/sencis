ba1f4261ad4170a5d5270fb49aa57c4d826c07df
Ricardo Vavretchek <ricardo.vavretchek@hotmail.com>
2024-04-13 11:59:13 -0300

    Render Ready

--- sencis/settings.py
+++ sencis/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,6 +78,7 @@ WSGI_APPLICATION = 'sencis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -87,8 +89,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://sa:GWG33Ge7G1Zc1SP99e229QRtfDaMQREJ@dpg-co66p6gl5elc73acse7g-a/sencis',
+        conn_max_age=600)
+}
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -124,8 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
