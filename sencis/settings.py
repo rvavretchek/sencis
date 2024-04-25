bad4b01cd16f6ddbbb440a2efde905e1b28a9394
Ricardo Vavretchek <ricardo.vavretchek@hotmail.com>
2024-04-25 15:14:58 -0300

    Prod Final 2024.04.25.15.15
    Alteração do tema da página admin

--- sencis/settings.py
+++ sencis/settings.py
@@ -33,6 +33,12 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'core'
+    
+    'django_adminlte',
+    'django_adminlte_theme',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core'
 ]
 
 MIDDLEWARE = [
